ffd6ea04fd1adf2dd8c5434e9b7dc252e650dae5
praak <praakrit@gmail.com>
2018-03-01 01:27:56 -0500

    Looping through all categories in baahrakhari.com

--- webscrape.py
+++ webscrape.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python
 import requests
+import time
+import os  
 from bs4 import BeautifulSoup as soup
-from selenium import webdriver
-from article import Article
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options
 
+from article import Article
 # All articles go into this list, which holds Article Objects. 
 articleList = []
 
-def parsing_Site(url):
-	# browser.get(url)
-	# innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
+def parsing_Site(urls):
+	count = 0
+	for url in urls:
+		# chrome_options = Options()  
+		# chrome_options.add_argument("--headless")  
+		# browser = webdriver.Chrome(chrome_options=chrome_options)
+		browser = webdriver.Chrome()
+		# browser.set_window_size(1120, 550)
+		browser.implicitly_wait(10)
+		print(url)
+		browser.get(url)
+		innerHTML = browser.execute_script("return document.body.innerHTML") 
 
-	# with open('output1.txt','wb') as myfile:
-	# 	myfile.write(str(innerHTML))
+		#returns the inner HTML as a string
+		time.sleep(.200)
+		with open('output{0}.txt'.format(count),'w') as myfile:
+			for line in innerHTML:
+				myfile.write(line)
+		time.sleep(.200)
+		with open('output{0}.txt'.format(count), 'r') as myfile:
+		    data=myfile.read().replace('\n', '')
+		
+		page_soup = soup(data, 'html.parser')
+		count += 1
 
-	with open('innerHTML.txt', 'r') as myfile:
-	    data=myfile.read().replace('\n', '')
-	page_soup = soup(data, 'html.parser')
-
-	baahrakhari(page_soup)
+		browser.close()
+		baahrakhari(page_soup)
 
 def baahrakhari(page_soup):
+	
 	site_section = page_soup.find(id="site-section")
 	news_items = site_section.find_all(class_="page-news-list")
 	news_1 = news_items[0]
@@ -64,6 +84,7 @@ def articleObjectTest():
 def makeArticleObject(source,title,description,link,imgLink):
 	article = Article(title, description, link, imgLink, source)
 	articleList.append(article)
+	print('-Article')
 
 def main():
 	# title_ = []
@@ -71,16 +92,13 @@ def main():
 	# imageLink_ = []
 	# description_ = []
 
-	#browser = buildDriver()
-	# browser = webdriver.Chrome()
-	# browser.implicitly_wait(10)
-
-	b_baseUrl = 'http://baahrakhari.com/'
+	b12_baseUrl = 'http://baahrakhari.com/'
 	b12_categories = ['special-news/1/%E0%A4%B5%E0%A4%BF%E0%A4%B6%E0%A5%87%E0%A4%B7','news-article/1/Politics','news-article/2/Economy','news-article/3/Sports','news-article/4/Interview','news-article/6/Thought','news-article/7/Country','news-article/8/12khari-samikchya','news-article/9/Literature','news-article/12/Blog','news-article/14/Editorial','news-article/15/Foreign']
-	b12_sites = [urls + b_baseUrl for urls in b12_categories]
-	parsing_Site(b12_sites)
+	b12_url = [b12_baseUrl + x for x in b12_categories]
+	parsing_Site(b12_url)
 
-	articleObjectTest()
+	# articleObjectTest()
+	print(len(articleList))
 
 
 if __name__== "__main__":
